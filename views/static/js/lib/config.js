ff7ffdcd6d5969faa520bd46acd82155d3f07573
momo <l>
2017-09-12 19:00:02 +0800

    登录页面与模块化

---
+++ views/static/js/lib/config.js
@@ -0,0 +1,11 @@
+/**
+ * Created by Nicole on 2017/9/11.
+ */
+require.config({
+  baseUrl:"/views/assets",
+  paths:{
+    jquery:"./jquery/jquery",
+    cookie:"./jquery-cookie/jquery.cookie",
+    template:"./artTemplate/template",
+  }
+})
